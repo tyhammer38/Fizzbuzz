0d093623c9a4509b182d7abbc71e899440d06fc6
bcht201 <brianting1227@gmail.com>
2019-11-07 14:54:41 +0000

    added fizz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,8 @@
 function(number){
-    if{number % 3 === 0 && number % 5 === 0}
-    return FizzBuzz;
+    if(number % 3 === 0 && number % 5 === 0){
+        return FizzBuzz;
+    }
+    if(number %3 === 0){
+        return Fizz;
+    }
 }
